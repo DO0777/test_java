7fbbe1db02c3bbb224561e494f9e964c5d0655f3
parkdoyoung <do1007y@naver.com>
2022-10-25 11:53:33 +0900

    MainClass06 added

--- Step03_Class/src/test/main/MainClass06.java
+++ Step03_Class/src/test/main/MainClass06.java
@@ -4,11 +4,13 @@ import test.mypac.Rect;
 
 public class MainClass06 {
 	public static void main(String[] args) {
+		//폭과 높이가 10, 20 인 사각형의 넚이
 		Rect r1 = new Rect();
 		r1.width = 10;
 		r1.height = 20;
 		r1.showArea();
 		
+		//폭과 높이가 5, 10 인 사각형의 넓이
 		Rect r2 = new Rect();
 		r2.width = 5;
 		r2.height = 10;
